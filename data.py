054cdc7ebc317380453accc2774cda2b587e5af7
Benjamin Walkenhorst <krylon@posteo.de>
2025-04-21 17:50:33 +0200

    Start defining data model

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-03-18 21:47:22 krylon>
+# Time-stamp: <2025-04-21 15:06:18 krylon>
 #
 # /data/code/python/medusa/data.py
 # created on 18. 03. 2025
@@ -16,6 +16,51 @@ medusa.data
 (c) 2025 Benjamin Walkenhorst
 """
 
+from abc import ABC
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import NamedTuple, Optional
+
+
+@dataclass(kw_only=True)
+class Host:
+    """Host represents a computer - real or virtual - on a network."""
+
+    host_id: int
+    name: str
+    os: str
+    last_contact: datetime
+
+
+@dataclass(slots=True)
+class Record(ABC):
+    """Record is the base class for data points collected on a Host."""
+
+    record_id: int = -1
+    host_id: int = 0
+    timestamp: datetime = field(default_factory=datetime.now)
+
+
+@dataclass(slots=True)
+class CPURecord(Record):
+    """CPURecord represents the CPU frequency and possible load."""
+
+    frequency: int = 0
+
+
+class SysLoad(NamedTuple):
+    """SysLoad represents the system load average commonly used on Un*x"""
+
+    load1: float
+    load5: float
+    load15: float
+
+
+@dataclass(slots=True, kw_only=True)
+class LoadRecord(Record):
+    """LoadRecord represents a system load measurement."""
+
+    load: Optional[SysLoad]
 
 
 # Local Variables: #
