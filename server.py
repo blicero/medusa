dc4632bbac60f0660bac34a21e626f9f65aad95c
Benjamin Walkenhorst <krylon@posteo.de>
2025-04-29 18:53:42 +0200

    Get server into a vaguely testable state

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-04-29 10:19:37 krylon>
+# Time-stamp: <2025-04-29 18:48:03 krylon>
 #
 # /data/code/python/medusa/server.py
 # created on 18. 03. 2025
@@ -19,10 +19,12 @@ medusa.server
 
 import json
 import logging
+import sys
 from socket import socket
-from socketserver import DatagramRequestHandler
+from socketserver import DatagramRequestHandler, ThreadingUDPServer
 
 from medusa import common
+from medusa.common import MedusaError
 from medusa.data import Host
 from medusa.database import Database
 from medusa.proto import Message, MsgType
@@ -42,29 +44,44 @@ class RequestHandler(DatagramRequestHandler):
 
     def handle(self) -> None:
         """You got a Datagram. Deal with it."""
-        self.log.debug(f"Handle request from {self.client_address}: {self.request[0]}")
+        self.log.debug("Handle request from %s: %s",
+                       self.client_address,
+                       self.request[0])
         packet = self.request[0].strip()
         conn: socket = self.request[1]
         msg = json.loads(packet)
         assert isinstance(msg, Message)
-        status: bool = False
         response: Message = Message(MsgType.Nothing, None)
 
-        match msg:
-            case (MsgType.NodeRegister, host):
-                assert isinstance(host, Host)
-                with self.db:
-                    self.db.host_add(host)
-                    status = True
-            case (mtype, payload):
-                self.log.debug("Don't know how to handle message type %s (%s) from %s",
-                               mtype,
-                               payload,
-                               self.client_address)
+        try:
+            match msg:
+                case (MsgType.NodeRegister, host):
+                    assert isinstance(host, Host)
+                    with self.db:
+                        self.db.host_add(host)
+                case (mtype, payload):
+                    self.log.debug("Don't know how to handle message type %s (%s) from %s",
+                                   mtype,
+                                   payload,
+                                   self.client_address)
+        except MedusaError as err:
+            self.log.error("%s while handling client request: %s",
+                           err.__class__.__name__,
+                           err)
+            response = Message(MsgType.Error,
+                               f"Error handling request: {err}")
 
         xfr: str = json.dumps(response)
-        conn.sendto(xfr, self.client_address)
+        conn.sendto(bytes(xfr, 'UTF-8'), self.client_address)
 
+
+if __name__ == '__main__':
+    try:
+        with ThreadingUDPServer(('::', common.PORT), RequestHandler) as srv:
+            srv.serve_forever()
+    except KeyboardInterrupt:
+        print("Bye bye")
+        sys.exit(0)
 
 # Local Variables: #
 # python-indent: 4 #
