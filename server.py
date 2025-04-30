6c3af46d364d650cbe36e0d0e4839fead5187dd5
Benjamin Walkenhorst <krylon@posteo.de>
2025-04-30 22:59:24 +0200

    Start working on the new Server

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-04-29 18:48:03 krylon>
+# Time-stamp: <2025-04-30 22:59:11 krylon>
 #
 # /data/code/python/medusa/server.py
 # created on 18. 03. 2025
@@ -17,71 +17,98 @@ medusa.server
 """
 
 
-import json
 import logging
-import sys
-from socket import socket
-from socketserver import DatagramRequestHandler, ThreadingUDPServer
+import socket
+import threading
+from dataclasses import dataclass
 
 from medusa import common
-from medusa.common import MedusaError
-from medusa.data import Host
 from medusa.database import Database
-from medusa.proto import Message, MsgType
 
 
-class RequestHandler(DatagramRequestHandler):
-    """Handles requests. Aren't you sorry you asked?"""
+class Server:
+    """Server handles the server side of the application."""
 
-    db: Database
+    __slots__ = [
+        "addr",
+        "port",
+        "log",
+        "lock",
+        "sock",
+        "active",
+    ]
+
+    addr: str
+    port: int
     log: logging.Logger
+    lock: threading.Lock
+    sock: socket.socket
+    active: bool
 
-    def setup(self) -> None:
-        """Prepare the handler for handling its request."""
+    def __init__(self, addr: str = "[::]", port: int = common.PORT) -> None:
+        self.addr = addr
+        self.port = port
+        self.log = common.get_logger("Server")
+        self.active = True
+        self.lock = threading.Lock()
+
+        dual: bool = socket.has_dualstack_ipv6()
+
+        self.sock = socket.create_server((addr, port),
+                                         family=socket.AF_INET6,
+                                         dualstack_ipv6=dual)
+
+    def is_active(self) -> bool:
+        """Return the Server's active flag."""
+        with self.lock:
+            return self.active
+
+    def stop(self) -> None:
+        """Clear the Server's active flag."""
+        with self.lock:
+            self.active = False
+
+    def listen(self) -> None:
+        """Accept incoming connections."""
+        while self.is_active():
+            conn, addr = self.sock.accept()
+            self.log.debug("Incoming connection from %s", addr)
+            # Create a new thread to handle the connection
+
+    def _handle(self, conn, addr):
+        """Deal with the client."""
+        handler = ConnectionHandler(conn, addr)
+        worker: threading.Thread = threading.Thread(target=handler.run, daemon=True)
+        worker.start()
+
+
+class ConnectionHandler:
+    """Connection handles the communication with an Agent."""
+
+    __slots__ = [
+        "log",
+        "db",
+        "conn",
+        "addr",
+    ]
+
+    log: logging.Logger
+    db: Database
+    conn: socket.socket
+    addr: tuple[str, int]
+
+    def __init__(self, conn: socket.socket, addr: tuple[str, int]) -> None:
+        self.log = common.get_logger("Connection")
         self.db = Database()
-        self.log = common.get_logger("server")
-        self.log.debug("One RequestHandler coming up. Banzai!")
-
-    def handle(self) -> None:
-        """You got a Datagram. Deal with it."""
-        self.log.debug("Handle request from %s: %s",
-                       self.client_address,
-                       self.request[0])
-        packet = self.request[0].strip()
-        conn: socket = self.request[1]
-        msg = json.loads(packet)
-        assert isinstance(msg, Message)
-        response: Message = Message(MsgType.Nothing, None)
-
-        try:
-            match msg:
-                case (MsgType.NodeRegister, host):
-                    assert isinstance(host, Host)
-                    with self.db:
-                        self.db.host_add(host)
-                case (mtype, payload):
-                    self.log.debug("Don't know how to handle message type %s (%s) from %s",
-                                   mtype,
-                                   payload,
-                                   self.client_address)
-        except MedusaError as err:
-            self.log.error("%s while handling client request: %s",
-                           err.__class__.__name__,
-                           err)
-            response = Message(MsgType.Error,
-                               f"Error handling request: {err}")
-
-        xfr: str = json.dumps(response)
-        conn.sendto(bytes(xfr, 'UTF-8'), self.client_address)
+        self.conn = conn
+        self.addr = addr
+        self.log.debug("About to handle connection from %s:%d",
+                       addr[0],
+                       addr[1])
 
 
-if __name__ == '__main__':
-    try:
-        with ThreadingUDPServer(('::', common.PORT), RequestHandler) as srv:
-            srv.serve_forever()
-    except KeyboardInterrupt:
-        print("Bye bye")
-        sys.exit(0)
+    def run(self) -> None:
+        """The main loop, so to speak."""
 
 # Local Variables: #
 # python-indent: 4 #
