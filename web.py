ece7f243eb910f3eb14868cb3e324f598660175d
Benjamin Walkenhorst <krylon@posteo.de>
2025-05-06 15:31:56 +0200

    WebUI - still baby steps, but it's going well so far

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-05-06 14:56:25 krylon>
+# Time-stamp: <2025-05-06 15:31:13 krylon>
 #
 # /data/code/python/medusa/web.py
 # created on 05. 05. 2025
@@ -19,14 +19,42 @@ medusa.web
 
 import logging
 import os
+import re
 import threading
 from datetime import datetime
+from typing import Final
 
 import bottle
-from bottle import route, run
+from bottle import response, route, run
 from jinja2 import Environment, FileSystemLoader
 
 from medusa import common
+
+mime_types: Final[dict[str, str]] = {
+    ".css":  "text/css",
+    ".map":  "application/json",
+    ".js":   "text/javascript",
+    ".png":  "image/png",
+    ".jpg":  "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".webp": "image/webp",
+    ".gif":  "image/gif",
+    ".json": "application/json",
+    ".html": "text/html",
+}
+
+suffix_pat: Final[re.Pattern] = re.compile("([.][^.]+)$")
+
+
+def find_mime_type(path: str) -> str:
+    """Attempt to determine the MIME type for a file."""
+    m = suffix_pat.search(path)
+    if m is None:
+        return "application/octet-stream"
+    suffix = m[1]
+    if suffix in mime_types:
+        return mime_types[suffix]
+    return "application/octet-stream"
 
 
 class WebUI:
@@ -41,13 +69,14 @@ class WebUI:
         self.log = common.get_logger("WebUI")
         self.lock = threading.Lock()
         if root == "":
-            self.root = os.path.join(".", "web", "templates")
+            self.root = os.path.join(".", "web")
         else:
             self.root = root
-        self.env = Environment(loader=FileSystemLoader(self.root))
+        self.env = Environment(loader=FileSystemLoader(os.path.join(self.root, "templates")))
 
         bottle.debug(common.DEBUG)
         route("/main", callback=self.main)
+        route("/static/<path>", callback=self.staticfile)
 
     def run(self) -> None:
         """Run the web server."""
@@ -58,6 +87,17 @@ class WebUI:
         tmpl = self.env.get_template("main.jinja")
         return tmpl.render(title=f"{common.APP_NAME} {common.APP_VERSION} - Main",
                            year=datetime.now().year)
+
+    def staticfile(self, path):
+        """Return one of the static files."""
+        # TODO Determine MIME type?
+        #      Set caching header?
+        mtype = find_mime_type(path)
+        response.set_header("Content-Type", mtype)
+
+        full_path = os.path.join(self.root, "static", path)
+        with open(full_path, "rb") as fh:
+            return fh.read()
 
 
 if __name__ == '__main__':
