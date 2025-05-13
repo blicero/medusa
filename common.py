0bee4b487f2970caa5856223ab36c4fe1039a178
Benjamin Walkenhorst <krylon@posteo.de>
2025-05-13 17:45:20 +0200

    Bump version number to 0.0.3

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-05-06 22:50:13 krylon>
+# Time-stamp: <2025-05-13 17:45:16 krylon>
 #
 # /data/code/python/medusa/common.py
 # created on 24. 01. 2024
@@ -23,7 +23,7 @@ from typing import Final
 from threading import Lock
 
 APP_NAME: Final[str] = "Medusa"
-APP_VERSION: Final[str] = "0.0.2"
+APP_VERSION: Final[str] = "0.0.3"
 DEBUG: Final[bool] = True
 TIME_FMT: Final[str] = "%Y-%m-%d %H:%M:%S"
 PORT: Final[int] = 9630
