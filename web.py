2fe1b0ffbbd8eae3425cdacfba2ff47746dfe9c2
Benjamin Walkenhorst <krylon@posteo.de>
2025-05-20 18:47:12 +0200

    Comment out WebUI.host_sensor_graph temporarily to pacify linter

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-05-19 19:07:30 krylon>
+# Time-stamp: <2025-05-20 18:36:13 krylon>
 #
 # /data/code/python/medusa/web.py
 # created on 05. 05. 2025
@@ -171,17 +171,17 @@ class WebUI:
         finally:
             db.close()
 
-    def host_sensor_graph(self, host_id: int) -> Union[bytes, str]:
-        """Render a time series chart of sensor data (i.e. temperature)."""
-        try:
-            db = Database()
-            host: Optional[data.Host] = db.host_get_by_id(host_id)
-            if host is None:
-                response.status = 404
-                return f"Host {host_id} does not exist in the database."
-            records = db.record_get_by_host_probe(host, "sensors")
-        finally:
-            db.close()
+    # def host_sensor_graph(self, host_id: int) -> Union[bytes, str]:
+    #     """Render a time series chart of sensor data (i.e. temperature)."""
+    #     try:
+    #         db = Database()
+    #         host: Optional[data.Host] = db.host_get_by_id(host_id)
+    #         if host is None:
+    #             response.status = 404
+    #             return f"Host {host_id} does not exist in the database."
+    #         records = db.record_get_by_host_probe(host, "sensors")
+    #     finally:
+    #         db.close()
 
     # Static files
 
